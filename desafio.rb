9d8277c3780c533eecd7721b9b163f665c641ebf
Pedro Mendonça <pedroh36mendonca@gmail.com>
2023-10-18 13:11:36 -0300

    Primeiro desafio feito

---
+++ desafio.rb
@@ -0,0 +1,7 @@
+print "Digite sue nome: "
+nome = gets.chomp
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+print "Digite sua idade: "
+idade = gets.chomp.to_i
+puts "Olá #{nome} #{sobrenome} você #{idade} anos de idade!"
